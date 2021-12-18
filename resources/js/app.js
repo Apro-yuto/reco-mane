956a1febc884a53c2147dabbce4de08a1ba05da4
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2021-12-18 20:06:35 +0900

    [Fix] Bootstrapは使用しないため記述削除、app.js修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+import App from '@/js/components/App.vue'
+import Vue from 'vue'
+
+const app = new Vue({
+    el: '#app',
+    components: {App},
+    template: '<App />',
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{title}}App</p>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data: () => {
+      return {
+        title: '議事録管理'
+      }
+    }
+  }
+</script>
